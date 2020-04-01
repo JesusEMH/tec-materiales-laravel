b50653dc66b6582d7bb0597c8eb7ab9a4b79871c
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-04-01 17:47:57 -0500

    no se que poner

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,12 @@
 <?php
 
-
-use App\User;
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\bcrypt;
+
 
 class UserController extends Controller
 {
