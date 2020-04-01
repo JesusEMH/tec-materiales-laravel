1b6e02594169de95c9e7e323ed2d6461dd5c5ed3
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-04-01 17:09:51 -0500

    dfdsfs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
 
   public function index(){
-    $users = User::all(); 
+    $users = \User::all(); 
 
     return response()->json([
       'status' => 'success',
