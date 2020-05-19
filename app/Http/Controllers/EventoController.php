c861269f234e4be597ffd974500bc715bdb4cb3f
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-05-19 16:47:25 -0500

    filtrado de mes en desarrollo 4

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -145,7 +145,7 @@ class EventoController extends Controller
     public function getPorMes()
     {
        
-        $evento = Evento::whereMonth('fecha', '05')->get()->orderBy('fecha', 'desc')->paginate(10);
+        $evento = Evento::whereMonth('fecha', '05')->orderBy('fecha', 'desc')->paginate(10)->get();
 
         if($evento){
             return response()->json(
