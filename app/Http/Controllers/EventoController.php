38d4a9927b3e8dbfea3666781ed4d5e871439c34
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-05-19 19:08:50 -0500

    filtrado de mes en desarrollo 16

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -147,7 +147,7 @@ class EventoController extends Controller
         $date_start = date('Y-m-01');
         $date_end = date('Y-m-31');
        
-        $eventos = Evento::whereBetween('fecha', [$date_start, $date_end])->paginate(10);
+        $eventos = Evento::whereBetween('fecha', [$date_start, $date_end])->orderBy('fecha', 'desc')->paginate(10);
 
 
         if($eventos){
