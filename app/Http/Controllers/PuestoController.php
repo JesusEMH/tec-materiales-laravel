31e357cff1ec7499ba41d9d259c22ac9af4f71c9
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-05-25 22:17:18 -0500

    permisobyuser 5

--- app/Http/Controllers/PuestoController.php
+++ app/Http/Controllers/PuestoController.php
@@ -148,7 +148,7 @@ class PuestoController extends Controller
     public function byUser($id)
     {
         
-        $puestos = Puesto::with(['user', 'departamento', 'cargo']);
+        $puestos = Puesto::with(['user', 'departamento', 'cargo'])->get();
 
         if($puestos){
             return response()->json([
