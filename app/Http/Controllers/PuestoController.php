2231c0ebd10f94ee915271145fea5c5f7c0c72c4
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-05-25 22:18:33 -0500

    permisobyuser 6

--- app/Http/Controllers/PuestoController.php
+++ app/Http/Controllers/PuestoController.php
@@ -148,7 +148,7 @@ class PuestoController extends Controller
     public function byUser($id)
     {
         
-        $puestos = Puesto::with(['user', 'departamento', 'cargo'])->get();
+        $puestos = Puesto::with(['user', 'departamento', 'cargo'])->where('usuario_id', $id);
 
         if($puestos){
             return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::group(['middleware' =>  'auth:api'], function(){
 	Route::apiResource('departamentos', 'DepartamentoController');	
 	Route::apiResource('ubications', 'UbicationController');
 
-	Route::get('puestobyuser', 'PuestoController@byUser');
+	Route::get('puestobyuser/{id}', 'PuestoController@byUser');
 
 	Route::delete('delete/{id}', 'UserController@delete');
 
